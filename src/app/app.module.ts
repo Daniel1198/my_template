ec5f0539492f8d40f9d87273025a7337cdfdea50
unknown <RTI@2PLT-LAP-DCM-JM.SIRTI.ADDS>
2023-02-02 18:40:15 +0000

    mise en place du panel ainsi que du style du tableau

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DocComponent } from './doc/doc.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DocComponent
+    DocComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
